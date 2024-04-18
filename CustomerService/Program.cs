94c01a2bda8c365b1d0d3309d76474bbe1b9251a
saba.fadhl <sabafadl98@gmail.com>
2024-04-19 01:16:19 +0300

    fix: routing for index

--- CustomerService/Program.cs
+++ CustomerService/Program.cs
@@ -6,14 +6,13 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<MasterContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("pglConnectionString") ??
-throw new InvalidOperationException("Connections string: pglConnectionString was not found"))); 
+throw new InvalidOperationException("Connections string: pglConnectionString was not found")));
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddCors(options =>
@@ -27,23 +26,38 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-// Apply CORS policy
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
 app.UseCors("AllowAll");
-//Seed Date
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+
+    // Add this line to redirect from "/" to the Swagger UI
+    endpoints.MapGet("/", context => { context.Response.Redirect("/swagger"); return Task.CompletedTask; });
+});
+
+app.MapControllers();
+
+//Seed Data
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
     SeedData.Initialize(services);
 }
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
 
 app.Run();
