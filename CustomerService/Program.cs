4975998e7bf98e2547112050035f553786467564
SabaFadhl <80360074+SabaFadhl@users.noreply.github.com>
2024-04-19 17:25:17 +0300

    Revert "feat: docker without db"

--- CustomerService/Program.cs
+++ CustomerService/Program.cs
@@ -6,58 +6,34 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<MasterContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("pglConnectionString") ??
-throw new InvalidOperationException("Connections string: pglConnectionString was not found")));
+throw new InvalidOperationException("Connections string: pglConnectionString was not found"))); 
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-builder.Services.AddCors(options =>
-    options.AddPolicy("AllowAll",
-        builder => builder
-            .AllowAnyOrigin()
-            .AllowAnyHeader()
-            .AllowAnyMethod()
-    )
-);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.UseCors("AllowAll");
-
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapControllerRoute(
-        name: "default",
-        pattern: "{controller=Home}/{action=Index}/{id?}");
-
-    // Add this line to redirect from "/" to the Swagger UI
-    endpoints.MapGet("/", context => { context.Response.Redirect("/swagger"); return Task.CompletedTask; });
-});
-
-app.MapControllers();
-
-//Seed Data
+//Seed Date
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
     SeedData.Initialize(services);
 }
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
